e50b43288e6cdf4d40899b26ffa699e6f6c0c05b
pgendreau <pgendreau@gmail.com>
2024-08-27 23:13:31 -0400

    commit initial changes

---
+++ packages/hardhat/contracts/DistributePrizes.sol
@@ -0,0 +1,58 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+interface IERC1155 {
+    function balanceOf(address _owner, uint256 _id) external view returns (uint256);
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+    function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external;
+}
+
+contract DistributePrizes {
+    function distribute(IERC1155 token, address[] calldata recipients, uint256[] calldata ids, uint256[][] calldata amounts, bytes calldata data) external {
+        require(token.isApprovedForAll(msg.sender, address(this)), "Sender has not approved disperse contract");
+        require(recipients.length == amounts.length, "Invalid input: recipients and amounts must have the sanme number of elements");
+
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint256 preBalance = token.balanceOf(msg.sender, ids[i]);
+            uint256 reqBalance;
+            for (uint256 j = 0; j < amounts.length; j++) {
+                require(amounts[j].length == ids.length, "Invalid input: the number of amounts does not match the number of ids");
+                reqBalance += amounts[j][i];
+            }
+
+            require(reqBalance <= preBalance, "Insufficient balance");
+        }
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+
+            uint256 len;
+            for (uint256 j = 0; j < ids.length; j++) {
+                if (amounts[i][j] != 0) {
+                    len++;
+                }
+            }
+            
+            if (len > 0) {
+                uint256[] memory recIds = new uint256[](len);
+                uint256[] memory recAmounts = new uint256[](len);
+
+                // uint256[] memory recIds;
+                // uint256[] memory recAmounts;
+
+                uint256 index;
+                for (uint256 j = 0; j < ids.length; j++) {
+                    if (amounts[i][j] != 0) {
+                        recIds[index] = ids[j];
+                        recAmounts[index] = amounts[i][j];
+                        index++;
+                    }
+                }
+                token.safeBatchTransferFrom(msg.sender, recipients[i], recIds, recAmounts, data);
+
+                // if (recIds.length > 0) {
+                //     token.safeBatchTransferFrom(msg.sender, recipients[i], recIds, recAmounts, data);
+                // }
+            }
+        }
+    }
+}
---
+++ packages/hardhat/test/DistributePrizes.ts
@@ -0,0 +1,44 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { DistributePrizes } from "../typechain-types";
+
+import { forgeDiamondAbi } from "./abi/forge_diamond";
+
+describe("DistributePrizes", function () {
+  // We define a fixture to reuse the same setup in every test.
+  async function deployDistributePrizesFixture() {
+    const ForgeDiamondAddress = "0x4fDfc1B53Fd1D80d969C984ba7a8CE4c7bAaD442";
+    const ForgeDiamondOwnerAddress = "0x01F010a5e001fe9d6940758EA5e8c777885E351e";
+
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: ["0x01F010a5e001fe9d6940758EA5e8c777885E351e"],
+    });
+
+    const forgeAdmin = await hre.ethers.getSigner(ForgeDiamondOwnerAddress);
+
+    const forgeDiamond = await hre.ethers.getContractAt(forgeDiamondAbi, ForgeDiamondAddress);
+
+    const [owner] = await ethers.getSigners();
+
+    forgeDiamond
+      .connect(forgeAdmin)
+      .adminMintBatch(
+        owner.address,
+        [1, 2, 3, 4, 5, 6, 21, 7, 8, 9, 10, 11, 13, 14],
+        [100, 100, 100, 50, 50, 50, 50, 25, 25, 25, 10, 10, 5, 5],
+      );
+
+    const distributePrizesFactory = await ethers.getContractFactory("DistributePrizes");
+    const distributePrizes = (await distributePrizesFactory.deploy()) as DistributePrizes;
+    await distributePrizes.waitForDeployment();
+
+    return { distributePrizes, forgeDiamond, owner };
+  }
+
+  it("Should have minted 100 of the first token id", async function () {
+    const { forgeDiamond, owner } = await loadFixture(deployDistributePrizesFixture);
+    expect(await forgeDiamond.balanceOf(owner.address, 1)).to.equal(100);
+  });
+});
